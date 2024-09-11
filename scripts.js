63c1c6c50b9ad0bef65d3bb44d65dfc82f0766d3
tharoonG17 <152915140+tharoonG17@users.noreply.github.com>
2024-09-11 20:16:28 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,8 @@
+// Wait for the window to load completely
+window.addEventListener('load', function () {
+    // Hide the loading screen
+    document.getElementById('loading-screen').style.display = 'none';
+    
+    // Show the main content
+    document.getElementById('content').style.display = 'block';
+});
